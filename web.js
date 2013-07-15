18964f86146790f5ee43eff90a87158b531c423d
Jaime Perez <jaime@yahoo.com>
2013-07-15 12:44:21 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var file = "index.html";
+var buf = fs.readFileSync(file);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
